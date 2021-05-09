8895a076f71ee48934f3b16345e34a84a8589384
ANKIT KUMAR <kumarankit2594@gmail.com>
2021-05-09 21:29:33 +0530

    Find last index of given element from starting

---
+++ List/FindLastIndexOfElement_Method2.py
@@ -0,0 +1,23 @@
+#last index - traversing from starting
+def lastIndex(arr,x):
+    l = len(arr)
+    if l == 0:
+        return -1
+    
+    smallerList = arr[1:]
+    smallerListOutput = lastIndex(smallerList,x)
+    if smallerListOutput == -1:
+        if arr[0] == x:
+            return 0
+        else:
+            return -1
+    else:
+        return smallerListOutput+1
+    
+# Main
+from sys import setrecursionlimit
+setrecursionlimit(11000)
+n=int(input())
+arr=list(int(i) for i in input().strip().split(' '))
+x=int(input())
+print(lastIndex(arr, x))
